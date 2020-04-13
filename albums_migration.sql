7cbc6b2cd78426bacaa0bb24fd4f175d5fa5c7c2
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-04-13 16:58:58 -0500

    first commit

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name VARCHAR(50) NOT NULL,
+    release_date INT NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(50) NOT NULL
+    PRIMARY KEY (id)
+);
