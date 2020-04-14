d68b006b0e9363653f4226ebcc76420dada4c8d5
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-04-14 16:15:41 -0500

    added data to table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,14 @@
+
 USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE',
-    name VARCHAR(50) NOT NULL,
-    release_date YEAR NOT NULL,
+    artist VARCHAR(150) DEFAULT 'NONE',
+    album_name VARCHAR(125) NOT NULL,
+    release_date INT(4) UNSIGNED NOT NULL,
     sales FLOAT NOT NULL,
-    genre VARCHAR(50) NOT NULL,
+    genre VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
---
+++ albums_seeder.sql
@@ -0,0 +1,35 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, album_name, release_date, sales, genre)
+VALUES  ('Michael Jackson', 'Thriller', 1982, 47.3, 'Pop' 'Rock' 'R&B'),
+        ('AC/DC', 'Back In Black', 1980, 26.1, 'Hard Rock'),
+        ('Pink Floyed', 'The Dark Side Of The Moon', 1973, 24.2, 'Progressive Rock'),
+        ('Meat Loaf', 'Bat Out Of Hell', 1977, 21.5, 'Hard Rock' 'Progressive Rock'),
+        ('Whitney Houston' 'Various Artist', 'The Bodyguard', 1992, 28.4, 'R&B' 'Soul' 'Pop' 'Soundtrack'),
+        ('Eagles', 'Their Greatest Hits 1971-1975', 1976, 32.2, 'Rock' 'Soft' 'Rock' 'Folk Rock'),
+        ('Bee Gees' 'Various Artist', 'Saturday Night Fever', 1977, 21.6, 'Disco' 'Soundtrack'),
+        ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft Rock'),
+        ('Various Artist', 'Grease: The Original Soundtrack From The Motion Picture', 1978, 14.4, 'Soundtrack'),
+        ('Led Zepplin', 'Led Zepplin IV', 1971, 29.0, 'Hard Rock' 'Heavy Metal' 'Folk'),
+        ('Michael Jackson', 'Bad', 1987, 19.3, 'Pop' 'Funk' 'Rock'),
+        ('Alanis Morissette', 'Jagged Little Pill', 1995, 24.4, 'Alternative Rock'),
+        ('Shania Twain', 'Come On Over', 1997, 29.6, 'Country' 'Pop'),
+        ('Celine Dion', 'Falling into You', 1996, 20.2, 'Pop' 'Soft Rock'),
+        ('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', 1967, 13.1, 'Rock'),
+        ('Eagles', 'Hotel California', 1976, 21.5, 'Rock' 'Soft Rock' 'Folk Rock'),
+        ('Various Artists', 'Dirty Dancing', 1987, 17.9, 'Pop' 'Rock' 'R&B'),
+        ('Adele', '21', 2011, 25.3, 'Pop' 'Soul'),
+        ('Celine Dion', 'Lets Talk About Love', 1997, 19.3, 'Pop' 'Soft Rock'),
+        ('The Beatles', '1', 2000, 22.6, 'Rock'),
+        ('Madonna', 'The Immaculate Collection', 1990, 19.4, 'Pop' 'Dance'),
+        ('The Beatles', 'Abbey Road', 1969, 14.4, 'Rock'),
+        ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 19.6, 'Rock'),
+        ('Dire Straits', 'Brothers in Arms', 1985, 17.7, 'Rock' 'Pop'),
+        ('James Horner', 'Titanic: Music from the Motion Picture', 1997, 18.1, 'Soundtrack'),
+        ('Metallica', 'Metallica', 1991, 21.2, 'Thrash Metal' 'Heavy Metal'),
+        ('Nirvana', 'Nevermind', 1991, 16.7, 'Grunge' 'Alternative Rock'),
+        ('Pink Floyd', 'The Wall', 1979, 17.6, 'Progressive Rock'),
+        ('Santana', 'Supernatural', 1999, 20.5, 'Rock'),
+        ('Guns N Roses', 'Appetite for Destruction', 1987, 21.6, 'Hard Rock');
+
+
